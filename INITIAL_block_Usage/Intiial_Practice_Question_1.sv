e96cae71dc6e880fa7f9be73655204cce41e3eed
Imaryanjha <149727541+Imaryanjha@users.noreply.github.com>
2025-10-04 16:27:03 +0530

    Update Intiial_Practice_Question_1.sv

--- INITIAL_block_Usage/Intiial_Practice_Question_1.sv
+++ INITIAL_block_Usage/Intiial_Practice_Question_1.sv
@@ -1,1 +1,20 @@
+//✔ Question : Assume System Consist of two global signals resetn and clk. Use an initial block to initialize clk to 1'b0 and resetn to 1'b0.
+//User must keep resetn in an active low state for 60 nSec at the start of the simulation and then make active high. Assume `timescale 1ns/1psShare your SV Code.
 
+`timescale 1ns/1ps
+
+module system_signals;
+
+  reg clk;
+  reg resetn;
+
+  initial begin
+    clk = 1'b0;
+    resetn = 1'b0;
+    #60;
+    resetn = 1'b1;
+  end
+
+  always #5 clk = ~clk;
+
+endmodule
