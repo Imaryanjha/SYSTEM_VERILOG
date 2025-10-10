a1a5e39e2f582eb4e9c56ec8a4467be825444451
Imaryanjha <149727541+Imaryanjha@users.noreply.github.com>
2025-10-10 18:59:41 +0530

    Update and rename Pass_By_Reference to Pass_By_Reference.sv

---
+++ Class_in_sv/Providing_Values/Pass_By_Reference.sv
@@ -0,0 +1,21 @@
+module tb;
+
+  task automatic swap (ref input bit [1:0]a,b);// function automatic bit [1:0] (add arguments);
+    bit [1:0]temp;
+    temp = a;
+    a=b;
+    b=temp;
+    $display("value of a : 0%d and b : %0d",a,b);
+  endtask
+
+  bit [1:0]a;
+  bit [1:0]b;
+  initial begin
+    a=1;
+    b=2;
+
+    swap(a,b);
+
+    $display("value of a:"%0d and b : %0d",a,b);
+    end
+     endmodule
